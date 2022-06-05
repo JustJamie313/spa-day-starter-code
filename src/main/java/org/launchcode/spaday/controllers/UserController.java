30696ccb5242cf6474614af9b27de0226129ca77
Jamie Miller <97189124+JustJamie313@users.noreply.github.com>
2022-06-05 10:59:40 -0500

    studio part 2 incomplete

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,7 +3,10 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -15,12 +18,14 @@ public class UserController {
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
+        model.addAttribute("user",new User());
+
+        if(errors.hasErrors()){
+            model.addAttribute("error",errors);
+            return "user/add";
+        }
+        if (user.getPassword().equals(user.getVerify())) {
            return "user/index";
         }
         else {
